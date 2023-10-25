5a0b0e94d0cfb06fe782f9ad24ba159b8c37232c
Rohmein <Rohmein@live.fr>
2023-10-25 20:01:00 +0200

    Création de la base de données des projets

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Projects from "../../database/projects.json";
 
 const PortfolioWrapper = styled.div`
   padding: 100px 0 100px;
@@ -26,19 +27,44 @@ const PortfolioTitle = styled.h2`
 `;
 
 const Line = styled.div`
-  background: #5b5963;
   width: 55px;
   height: 1px;
   margin: 30px auto;
+  background: #5b5963;
+`;
+
+const ProjectsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+
+const ProjectsCard = styled.div`
+  text-align: left;
+  background: #f5f5f7;
+  width: 100%;
+  height: 400px;
 `;
 
 const Portfolio = () => {
+  console.log(Projects);
   return (
     <PortfolioWrapper>
       <PortfolioTitle>
         PORTFOLIO<span>MES PROJETS</span>
       </PortfolioTitle>
       <Line></Line>
+      <ProjectsContainer>
+        {Projects.map((project) => (
+          <ProjectsCard key={project.id}>
+            <img src={project.image} alt="Projet" />
+            <h3>{project.title}</h3>
+            <p>{project.description}</p>
+            <p>{project.competences}</p>
+            {project.url}
+          </ProjectsCard>
+        ))}
+      </ProjectsContainer>
     </PortfolioWrapper>
   );
 };
