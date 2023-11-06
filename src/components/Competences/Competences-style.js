5053bfdc8ac58d78698ee112d191e7ef22d895f1
Rohmein <Rohmein@live.fr>
2023-11-06 09:37:55 +0100

    Responsive section "Compétences"

--- src/components/Competences/Competences-style.js
+++ src/components/Competences/Competences-style.js
@@ -14,6 +14,7 @@ export const CompetencesTitle = styled.h2`
   font-size: 30px;
   font-weight: 300;
   letter-spacing: 5px;
+
   & span {
     position: absolute;
     top: -40px;
@@ -23,6 +24,24 @@ export const CompetencesTitle = styled.h2`
     font-weight: 800;
     opacity: 0.06;
     z-index: 0;
+  }
+
+  @media (max-width: 720px) {
+    font-size: 25px;
+
+    & span {
+      top: -30px;
+      font-size: 60px;
+    }
+  }
+
+  @media (max-width: 565px) {
+    font-size: 14px;
+
+    & span {
+      top: -12px;
+      font-size: 28px;
+    }
   }
 `;
 
@@ -34,25 +53,20 @@ export const Line = styled.div`
 `;
 
 export const ToolsContainer = styled.div`
-  display: flex;
-  align-items: center;
+  margin: auto;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  gap: 15px;
+  width: 75%;
 
-  & #office {
-    width: 30%;
-    filter: blur(3px);
-  }
-
-  & #tools-cards {
-    width: 70%;
-    display: flex;
-    gap: 15px;
+  @media (max-width: 720px) {
+    grid-template-columns: 1fr;
   }
 `;
 
 export const ToolsCard = styled.div`
-  padding: 20px;
-  width: 60%;
-  display: flex;
+  padding: 30px;
+  width: 100%;
   background: white;
   box-shadow: -6px 10px 40px rgba(0, 0, 0, 0.06);
 `;
--- src/components/Competences/Competences.jsx
+++ src/components/Competences/Competences.jsx
@@ -7,7 +7,6 @@ import {
   ToolsContainer,
 } from "./Competences-style";
 import Tools from "../../database/tools.json";
-// import Office from "../../assets/images/Office.jpg";
 
 const Competences = () => {
   return (
@@ -17,14 +16,11 @@ const Competences = () => {
       </CompetencesTitle>
       <Line></Line>
       <ToolsContainer>
-        {/* <img src={Office} id="office" alt="Bureau" /> */}
-        <div id="tools-cards">
-          {Tools.map((tool) => (
-            <ToolsCard key={tool.id}>
-              <img src={tool.image} alt="Outil"></img>
-            </ToolsCard>
-          ))}
-        </div>
+        {Tools.map((tool) => (
+          <ToolsCard key={tool.id}>
+            <img src={tool.image} alt="Outil"></img>
+          </ToolsCard>
+        ))}
       </ToolsContainer>
     </CompetencesWrapper>
   );
